f5e3393e4d47ea8ece86f46e5c3cb0ca10fde945
wcrmie <ImWormTheKing@gmail.com>
2026-04-05 15:28:25 -0400

    made Game a singleton

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 
+// if we truly want a Game object it should be a singleton
+
 class Game{
 public:
+
+  static Game getInstance() {
+    return instance;
+  }
+
   void start(){
     std::cout << "Game starting..." << std::endl;
   }
+
+private:
+
+  static Game instance;
+
+  Game() = default;
+
 };
 
 //todo add in main game loop and box logic
