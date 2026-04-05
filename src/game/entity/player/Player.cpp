a0541693860a2731f8dc11bfb59fb39ac0db3522
Woodolph <166852620+MaxximilianC@users.noreply.github.com>
2026-04-05 18:51:43 -0400

    Implement damage and health logic in Player class

--- src/game/entity/player/Player.cpp
+++ src/game/entity/player/Player.cpp
@@ -1,3 +1,10 @@
 #include <iostream>
+#include "Player.h"
 
-
+void Player::takeDamage(int damage) {
+    m_health -= damage;
+    if (m_health < 0) {
+        m_health = 0;
+        std::cout << "Player has been defeated!" << std::endl;
+    }
+}
