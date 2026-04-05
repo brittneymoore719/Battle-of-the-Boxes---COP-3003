9ba4e344bde0f5b1ebdf47c286cca316c416b722
wcrmie <ImWormTheKing@gmail.com>
2026-04-05 11:42:27 -0400

    added different statistic types for Character class, as well as a Statistic enum for access

--- src/game/entity/Character.h
+++ src/game/entity/Character.h
@@ -3,27 +3,39 @@
 #ifndef BATTLE_OF_THE_BOXES___COP_3003_CHARACTER_H
 #define BATTLE_OF_THE_BOXES___COP_3003_CHARACTER_H
 #include <array>
+#include <vector>
 
 #include "../GameObject.h"
 
+enum Statistic{MAX_HP, CURRENT_HP, BASE_DMG, PIERCE, DEFENSE, DODGE, ACCURACY, CRIT_CHANCE, CRIT_MULTIPLIER};
 
 class Character : public GameObject {
     public:
 
-    Character();
+    Character(
+
+        double maxHp = 20.0,
+        double currentHp = 20.0,
+        double baseDmg = 5.0,
+        double pierce = 0.0,
+        double defense = 1.0,
+        double dodge = 0.0,
+        double accuracy = 0.7,
+        double critChance = 0.1,
+        double critMultiplier = 0.25
+
+
+        );
 
 public:
 
+    double getStatistic(Statistic stat) const;
 
     void update();
 
 private:
 
-
-
-
-
-    double m_health;
+    std::vector<double> m_statistics;
 
 };
 
