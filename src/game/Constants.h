85d89aa93d73e05411c6dc56663605865891ab40
Woodolph <166852620+MaxximilianC@users.noreply.github.com>
2026-04-05 18:55:15 -0400

    Add Constants.h for global game settings

---
+++ src/game/Constants.h
@@ -0,0 +1,10 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace GameConstants {
+    const int WINDOW_WIDTH = 800;
+    const int WINDOW_HEIGHT = 600;
+    const int MAX_PLAYER_HEALTH = 100;
+}
+
+#endif
