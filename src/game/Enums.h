8d9163825cc774195188e7ad88b06ef5e1079ee7
Woodolph <166852620+MaxximilianC@users.noreply.github.com>
2026-04-05 19:04:56 -0400

    Add Enums.h for game state and enemy types

---
+++ src/game/Enums.h
@@ -0,0 +1,16 @@
+#ifndef ENUMS_H
+#define ENUMS_H
+
+enum class GameState {
+    MAIN_MENU,
+    PLAYING,
+    GAME_OVER
+};
+
+enum class EnemyType {
+    ZOMBIE_WORKER,
+    MANAGER,
+    BEZOS_BOSS
+};
+
+#endif
