9ad5c97762c4e2a40507da9ee03cf5fd4eb6d0db
Woodolph <166852620+MaxximilianC@users.noreply.github.com>
2026-04-05 21:09:55 -0400

    Refactor PlayerCombatant act methods
    Removed the default act() method and updated the act(Character&) method.

--- src/game/combat/combatant/PlayerCombatant.h
+++ src/game/combat/combatant/PlayerCombatant.h
@@ -1,15 +1,17 @@
 //
 // Created by Ethan Pedrick on 4/5/2026.
 //
-
+//Edited by Woodolph Sylvain 4/5/06
 #ifndef BATTLE_OF_THE_BOXES_PLAYERCOMBATANT_H
 #define BATTLE_OF_THE_BOXES_PLAYERCOMBATANT_H
 #include "Combatant.h"
 
-class PlayerCombatant : public Combatant {
+class PlayerCombatant : public Combatant 
+{
+public:
+    explicit PlayerCombatant(Character& character);
 
-
-    void act() override;
+    void act(Character& target) override;
 
 };
 
