8bc3864ca4551adffdb73663d87ca39d34c0efa4
Woodolph <166852620+MaxximilianC@users.noreply.github.com>
2026-04-05 21:08:16 -0400

    Refactor Combatant constructor and method formatting

--- src/game/combat/combatant/Combatant.cpp
+++ src/game/combat/combatant/Combatant.cpp
@@ -1,15 +1,12 @@
 //
 // Created by Ethan Pedrick on 4/5/2026.
 //
-
+//Edited by Woodolph Sylvain 4/5/06
 #include "Combatant.h"
 
-Combatant::Combatant(Character& character) :
-    m_character(character)
-{}
+Combatant::Combatant(Character& character)
+    : m_character(character) {}
 
-void Combatant::act() {
-
-
-
+Character& Combatant::getCharacter() {
+    return m_character;
 }
