e6c43f2ba3c2d8ff49924ee838db4c0f78baeec6
Woodolph <166852620+MaxximilianC@users.noreply.github.com>
2026-04-05 21:07:12 -0400

    Change m_character access modifier to protected and make combatant an abstract base class
    Changed access modifier of m_character from private to protected.

--- src/game/combat/combatant/Combatant.h
+++ src/game/combat/combatant/Combatant.h
@@ -1,23 +1,26 @@
 //
 // Created by Ethan Pedrick on 4/5/2026.
 //
+//
+//Edited by Woodolph Sylvain 4/5/26
+//
 
 #ifndef BATTLE_OF_THE_BOXES_COMBATANT_H
 #define BATTLE_OF_THE_BOXES_COMBATANT_H
+
 #include "game/entity/Character.h"
 
 class Combatant {
-
 public:
+    explicit Combatant(Character& character);
+    virtual ~Combatant() = default;
 
-    Combatant(Character& character);
+    virtual void act(Character& target) = 0;
 
-    virtual void act();
+    Character& getCharacter();
 
-private:
-
-    const Character& m_character; // The character the combatant is related to
-
+protected:
+    Character& m_character;
 };
 
-#endif //BATTLE_OF_THE_BOXES_COMBATANT_H
+#endif
