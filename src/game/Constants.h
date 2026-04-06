b053020b0ccf9dae5b48c4202c990b018e97761d
brittneymoore719 <brittneymoore719@gmail.com>
2026-04-06 13:18:10 -0400

    Updated the constants that will be used in the game.

--- src/game/Constants.h
+++ src/game/Constants.h
@@ -1,10 +1,24 @@
+//Edited by Brittney Moore 4/6/2026
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
 namespace GameConstants {
+    //window
     const int WINDOW_WIDTH = 800;
     const int WINDOW_HEIGHT = 600;
-    const int MAX_PLAYER_HEALTH = 100;
-}
+
+    //player
+    const int MAX_PLAYER_HEALTH = 150;
+    const int PLAYER_SPEED = 5;
+    const int PLAYER_ATTACK_DAMAGE = 15;
+
+    //enemy
+    const int MAX_ENEMY_HEALTH = 100;
+    const int ENEMY_SPEED = 5;
+    const int ENEMY_ATACK_DAMAGE = 10;
+
+    //combat - levels for the player to complete 
+    const int MAX_FLOORS = 5;
+    }
 
 #endif
