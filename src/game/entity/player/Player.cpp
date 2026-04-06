a99b550db2c18d9eb0375d99cfe35d1878ea1dca
Woodolph <166852620+MaxximilianC@users.noreply.github.com>
2026-04-05 20:57:42 -0400

    Improve damage handling and defeat message

--- src/game/entity/player/Player.cpp
+++ src/game/entity/player/Player.cpp
@@ -2,9 +2,13 @@
 #include "Player.h"
 
 void Player::takeDamage(int damage) {
+    if (damage < 0) {
+        throw GameException("Damage cannot be negative.");
+    }
+
     m_health -= damage;
     if (m_health < 0) {
         m_health = 0;
-        std::cout << "Player has been defeated!" << std::endl;
+        std::cout << m_name << " has been defeated!" << std::endl;
     }
 }
