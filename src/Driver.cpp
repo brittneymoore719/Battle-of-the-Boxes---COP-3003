5c395fd3f5b105ffa22f27fa5dca9622746d57f7
0Unique <75515676+0Unique@users.noreply.github.com>
2026-04-05 22:53:34 -0400

    updated driver showing stage functionality

--- src/Driver.cpp
+++ src/Driver.cpp
@@ -4,32 +4,11 @@
 #include <iostream>
 #include <string>
 int main() {
-
+    std::cout << "Starting main game loop\n";
 	while ( true )
 	{
-	    std::cout << "select an option:\n"
-					<< "1. quit\n"
-					<< "2. update current Stage\n"
-					<< "3. change current Stage\n";
-	    int choice{0};
-	    std::cin >> choice;
-		std::cout << "\n";
-		if (choice == 1) break;
-		if (choice == 2) StageController::updateStage();
-		if (choice == 3) {
-		    std::cout << "Select Stage to change to\n"
-						<< "1. Title\n";
-			int stageChoice{0};
-			std::cin >> stageChoice;
-			std::cout << "\n";
-			switch (stageChoice) {
-			case 1:
-    			StageController::changeStage(new TitleStage());
-    			break;
-			}
-		}
-
-		std::cout << "\n\n";
+	    // update current stage
+		StageController::updateStage();
 	}
     return 0;
 }
