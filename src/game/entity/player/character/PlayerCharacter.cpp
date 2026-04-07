c2a869f9d61744524957beb79d69d50e25f2a724
brittneymoore719 <brittneymoore719@gmail.com>
2026-04-07 00:37:03 -0400

    implementation of constructor in playerCharacter.h

--- src/game/entity/player/character/PlayerCharacter.cpp
+++ src/game/entity/player/character/PlayerCharacter.cpp
@@ -1,10 +1,17 @@
 //
 // Created by Ethan Pedrick on 4/1/26.
-//
+// Updated by Brittney Moore on 4/7/2026
 
 #include "PlayerCharacter.h"
 #include <iostream>
 #include "game/entity/Character.h"
+#include "game/Constants.h"
+
+PlayerCharacter::PlayerCharacter()
+    : Character(
+        GameConstants::MAX_PLAYER_HEALTH,
+        GameConstants::MAX_PLAYER_HEALTH,
+        GameConstants::PLAYER_ATTACK_DAMAGE) {}
 
 double PlayerCharacter::getHealth() {
     return getStatistic(Statistic::CURRENT_HP);
