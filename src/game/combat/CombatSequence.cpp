ffd4a6617f8e766a2a2ff612e2882bc695f09cfd
brittneymoore719 <brittneymoore719@gmail.com>
2026-04-06 23:52:07 -0400

    Added to CombatSequence, dealing with whose turn

--- src/game/combat/CombatSequence.cpp
+++ src/game/combat/CombatSequence.cpp
@@ -13,7 +13,18 @@ CombatSequence::CombatSequence(std::array<Combatant, 2> combatants):
 {}
 
 void CombatSequence::playRound() {
+    //get attacker and defender absed on whose turn it is
+    Combatant& attacker = m_combatant[m_turn];
+    Combatant& defender = m_combatant[!m_turn];
 
+    //attacker atacks defender
+    attacker.act(defender.getCharacter());
+
+    //switch
+    m_turn = !m_turn;
+
+    //add count to round counter
+    m_round++;
 
 
 }
