64f8cd8c249c5e200949e8cabf8d23fefd465c05
brittneymoore719 <brittneymoore719@gmail.com>
2026-04-06 23:38:34 -0400

    Implemented enemy class with base stats and isAlive method

--- src/game/entity/enemy/Enemy.h
+++ src/game/entity/enemy/Enemy.h
@@ -1,13 +1,25 @@
-
+//edited by Brittney Moore 4/6/2026
 
 #ifndef BATTLE_OF_THE_BOXES___COP_3003_ENEMY_H
 #define BATTLE_OF_THE_BOXES___COP_3003_ENEMY_H
 #include "../Character.h"
+#include <string>
 
 class Enemy : public Character {
+public:
+    Enemy(
+        std::string name, 
+        double maxHp = 30, 
+        double baseDmg = 8.0, 
+        double defense = 0.5,
+        double accuracy = 0.7
+    );
 
+    std::string getName() const;
+    bool isAlive() const;
 
-
+    private:
+    std::string m_name;
 };
 
 #endif //BATTLE_OF_THE_BOXES___COP_3003_ENEMY_H
