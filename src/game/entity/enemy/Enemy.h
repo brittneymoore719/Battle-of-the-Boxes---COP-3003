5ff77b7f6f586f9da43d11e38605612f0ed38e88
brittneymoore719 <brittneymoore719@gmail.com>
2026-04-07 00:01:07 -0400

    Connected Enums.h to Enemy.h so it can use enemyType

--- src/game/entity/enemy/Enemy.h
+++ src/game/entity/enemy/Enemy.h
@@ -3,12 +3,13 @@
 #ifndef BATTLE_OF_THE_BOXES___COP_3003_ENEMY_H
 #define BATTLE_OF_THE_BOXES___COP_3003_ENEMY_H
 #include "../Character.h"
+#include "../../Enums.h"
 #include <string>
 
 class Enemy : public Character {
 public:
-    Enemy(
-        std::string name, 
+    Enemy(std::string name, 
+        EnemyType type,
         double maxHp = 30, 
         double baseDmg = 8.0, 
         double defense = 0.5,
@@ -17,9 +18,11 @@ public:
 
     std::string getName() const;
     bool isAlive() const;
+    EnemyType getType() const;
 
     private:
     std::string m_name;
+     EnemyType m_type;
 };
 
 #endif //BATTLE_OF_THE_BOXES___COP_3003_ENEMY_H
