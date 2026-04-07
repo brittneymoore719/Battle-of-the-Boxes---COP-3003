c4eb0c68f9c428f1771ee28f852b5500f3d3e9ae
brittneymoore719 <brittneymoore719@gmail.com>
2026-04-06 23:31:29 -0400

    Setting base stats for enemy assing them up to character constructor

--- src/game/entity/enemy/Enemy.cpp
+++ src/game/entity/enemy/Enemy.cpp
@@ -1,4 +1,13 @@
+//edited by Brittney Moore 4/6/2026
 #include "../Character.h"
+#include "Enemy.h"
 
-//Enemy-specific logic will be held here
+Enemy::Enemy(std::string name, double maxHp, double baseDmg, double defense, double accuracy)
+    : Character(maxHp, maxHp, baseDmg, 0.0, defense, 0.0, accuracy),
+      m_name(name){}
+
+std::string Enemy::getName() const { return m_name; }
+
+bool Enemy::isAlive() const { return getStatistic(CURRENT_HP) > 0; }
+    //Enemy-specific logic will be held here
 //enemies will inherit health and movement from the character
