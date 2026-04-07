230029881525ed62461e50c76d934b1381976541
brittneymoore719 <brittneymoore719@gmail.com>
2026-04-07 00:14:08 -0400

    fixed battlestage to initalize enemy with name and type

--- src/display/stage/battle/BattleStage.h
+++ src/display/stage/battle/BattleStage.h
@@ -8,6 +8,7 @@
 #include "display/stage/Stage.h"
 #include "game/entity/player/character/PlayerCharacter.h"
 #include "game/entity/enemy/Enemy.h"
+#include "game/Enums.h"
 
 class BattleStage : public Stage {
 public:
@@ -16,7 +17,6 @@ public:
     virtual void update() override;
 private:
     PlayerCharacter m_player{};
-    Enemy m_enemy{};
-};
+ Enemy m_enemy{"Zombie Worker", EnemyType::ZOMBIE_WORKER};
 
 #endif //BATTLE_OF_THE_BOXES_BATTLESTAGE_H
