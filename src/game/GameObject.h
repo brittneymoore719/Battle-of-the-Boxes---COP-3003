bcb66727b63c4e2b52f187cf0c727b1a18ef246f
Woodolph <166852620+MaxximilianC@users.noreply.github.com>
2026-04-08 15:57:28 -0400

    Update GameObject.h to remove implementation and and fix public inheritance.

--- src/game/GameObject.h
+++ src/game/GameObject.h
@@ -1,32 +1,16 @@
-
 #ifndef BATTLE_OF_THE_BOXES___COP_3003_GAMEOBJECT_H
 #define BATTLE_OF_THE_BOXES___COP_3003_GAMEOBJECT_H
+
+#include <array>
 #include "../display/displayable/Displayable.h"
 
-
-class GameObject : Displayable {
-
+class GameObject : public Displayable {
 public:
-
-
-    GameObject(std::array<double,2> position = {0,0}):
-
-    m_position(position)
-
-    {
-
-    }
-
-
-    std::array<double, 2> getPositionVector() const {
-        return m_position;
-    }
-
+    explicit GameObject(std::array<double, 2> position = {0, 0});
+    std::array<double, 2> getPositionVector() const;
 
 private:
-
     std::array<double, 2> m_position;
-
 };
 
-#endif //BATTLE_OF_THE_BOXES___COP_3003_GAMEOBJECT_H
+#endif
