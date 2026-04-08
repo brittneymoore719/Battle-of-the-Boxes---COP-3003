696c953a0ff93440e166c4930a9c71453d0fc3cd
Woodolph <166852620+MaxximilianC@users.noreply.github.com>
2026-04-08 16:00:23 -0400

    Fix Displayable inheritance  and declarations

--- src/display/displayable/Displayable.h
+++ src/display/displayable/Displayable.h
@@ -1,35 +1,23 @@
-
 #ifndef BATTLE_OF_THE_BOXES___COP_3003_DISPLAYABLE_H
 #define BATTLE_OF_THE_BOXES___COP_3003_DISPLAYABLE_H
-#include <array>
 
+#include <array>
 #include "../Updatable.h"
 
-
-class Displayable : Updatable {
-
-
+class Displayable : public Updatable {
 public:
-
     Displayable(bool isDisplayed = false, std::array<double, 2> screenPos = {0.0, 0.0});
-
-    Displayable(Displayable& displayable);
+    Displayable(const Displayable& displayable);
 
     bool isDisplayed() const;
-
     void setDisplayed(bool isDisplayed);
-
-    std::array<double,2> getScreenPosition();
-
-    void setScreenPosition(std::array<double,2> position);
+    std::array<double, 2> getScreenPosition();
+    void setScreenPosition(std::array<double, 2> position);
+    void update() override;
 
 private:
-
     bool m_isDisplayed;
-
-    std::array<double,2> m_screenPosition{}; // where the thing is on the screen
-
-
+    std::array<double, 2> m_screenPosition{};
 };
 
-#endif //BATTLE_OF_THE_BOXES___COP_3003_DISPLAYABLE_H
+#endif
