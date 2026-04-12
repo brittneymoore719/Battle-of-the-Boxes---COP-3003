10702283a527ae19b779a9d5c62bfdbc462252b8
0Unique <75515676+0Unique@users.noreply.github.com>
2026-04-12 15:31:08 -0400

    change HealthPool ints to doubles

--- src/game/entity/HealthPool.cpp
+++ src/game/entity/HealthPool.cpp
@@ -1,14 +1,16 @@
 #include "HealthPool.h"
 #include <SFML/System/Vector2.hpp>
+#include <iostream>
 
-HealthPool::HealthPool(int max_hp) : m_max_health{max_hp}, m_current_health{max_hp}, m_block{0} {}
+HealthPool::HealthPool(double max_hp) : m_max_health{max_hp}, m_current_health{max_hp}, m_block{0} {}
 
 void HealthPool::draw(sf::Vector2f display_position) {
 
 }
 
-void HealthPool::takeDamage(int damageAmount) {
+void HealthPool::takeDamage(double damageAmount) {
     m_current_health -= damageAmount;
+    std::cout << "hp: " << m_current_health << "\n";
 }
 
 bool HealthPool::isDead() {
--- src/game/entity/HealthPool.h
+++ src/game/entity/HealthPool.h
@@ -5,17 +5,17 @@
 #include <SFML/System/Vector2.hpp>
 class HealthPool {
     public:
-    explicit HealthPool(int max_hp);
-    void takeDamage(int damageAmount);
+    explicit HealthPool(double max_hp);
+    void takeDamage(double damageAmount);
     bool isDead();
 
     void draw(sf::Vector2f display_position);
 
     private:
     sf::RectangleShape m_rect;
-    int m_max_health;
-    int m_current_health;
-    int m_block;
+    double m_max_health;
+    double m_current_health;
+    double m_block;
 };
 
 #endif
