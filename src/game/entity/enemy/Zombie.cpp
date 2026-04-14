b70681b0097da88c8f8bb4689c5fd3397e0dab3c
Woodolph <166852620+MaxximilianC@users.noreply.github.com>
2026-04-14 02:41:40 -0400

    Initialize Zombie name in constructor
    Set the name of the Zombie entity during initialization.

--- src/game/entity/enemy/Zombie.cpp
+++ src/game/entity/enemy/Zombie.cpp
@@ -1,7 +1,10 @@
 #include "Zombie.h"
 #include "game/Constants.h"
 
-Zombie::Zombie(int position) : Enemy(position, GameConstants::MAX_ENEMY_HEALTH, "sprites/zombie_worker.png") {}
+Zombie::Zombie(int position) : Enemy(position, GameConstants::MAX_ENEMY_HEALTH, "sprites/zombie_worker.png") 
+{
+    setName("Zombie");
+}
 
 void Zombie::attack(Character& target) {
     target.getHealthPool().takeDamage(GameConstants::ENEMY_ATTACK_DAMAGE);
