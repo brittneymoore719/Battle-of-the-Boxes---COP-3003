2cad3831f673539f8c287ef7fb342bfd1977d931
Woodolph <166852620+MaxximilianC@users.noreply.github.com>
2026-04-14 03:29:29 -0400

    Refactor TitleStage constructor and update method

--- src/display/stage/title/TitleStage.cpp
+++ src/display/stage/title/TitleStage.cpp
@@ -5,35 +5,37 @@
 #include "../../displayable/ui/button/UiButton.h"
 #include "game/Constants.h"
 #include "game/entity/enemy/Zombie.h"
+#include "WindowManager.h"
 #include <iostream>
 #include <memory>
 #include <vector>
-#include "WindowManager.h"
 
-TitleStage::TitleStage() : Stage(), startButton{"Start Game", {GameConstants::WINDOW_WIDTH / 2.f, GameConstants::WINDOW_HEIGHT / 4.f * 3.f}, {GameConstants::WINDOW_WIDTH / 2.f,GameConstants::WINDOW_HEIGHT / 4.f}} 
-  {GameConstants::WINDOW_WIDTH / 2.f, GameConstants::WINDOW_HEIGHT / 4.f}},
-      exitButton{"Exit", {GameConstants::WINDOW_WIDTH / 2.f, GameConstants::WINDOW_HEIGHT / 4.f * 3.f + 80.f},
+TitleStage::TitleStage()
+    : Stage(),
+      startButton{"Start Game",
+                  {GameConstants::WINDOW_WIDTH / 2.f, GameConstants::WINDOW_HEIGHT / 4.f * 3.f},
+                  {GameConstants::WINDOW_WIDTH / 2.f, GameConstants::WINDOW_HEIGHT / 4.f}},
+      exitButton{"Exit",
+                 {GameConstants::WINDOW_WIDTH / 2.f, GameConstants::WINDOW_HEIGHT / 4.f * 3.f + 80.f},
+                 {GameConstants::WINDOW_WIDTH / 2.f, GameConstants::WINDOW_HEIGHT / 4.f}}
 {
     std::cout << "TitleStage initialized\n";
 }
 
 void TitleStage::update() {
     startButton.update();
+    exitButton.update();
 
-    // change stage if start button was pressed
     if (startButton.hasBeenClicked()) {
         std::cout << "changing stage to BattleStage\n";
         std::vector<std::unique_ptr<Enemy>> enemies;
         enemies.push_back(std::make_unique<Zombie>(1));
-
         auto player = std::make_unique<PlayerCharacter>();
-
 
         StageController::changeStage(new BattleStage(std::move(enemies), std::move(player)));
     }
 
-    if (exitButton.hasBeenClicked()) 
-    {
+    if (exitButton.hasBeenClicked()) {
         WindowManager::getWindow().close();
     }
 }
