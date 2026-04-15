636c4da64dd72b518e1b8ac2f8a2d7b37eea999e
brittneymoore719 <brittneymoore719@gmail.com>
2026-04-15 16:09:40 -0400

    Added TurnManager.cpp

---
+++ src/game/combat/TurnManager.cpp
@@ -0,0 +1,15 @@
+#include "TurnManager.h"
+#include "WindowManager.h"
+#include "game/entity/enemy/Enemy.h"
+
+TurnManager::TurnManager(combatSequence& sequence)
+	: m_sequence(sequence),
+	m_cardsPlayed(0),
+	m_drawCounterText(WindowManager::getFont())
+{
+	m_drawCounterText.setFont(WindowManager::getFont());
+	m_drawCounterText.setCharacterSize(20);
+	m_drawCounterText.setFillColor(sf::Color::White);
+	m_drawCounterText.setPosition({10.f, 10.f});
+    updateDrawCounterDisplay();
+}
